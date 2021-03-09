5b3b83313cdf21bdaf10609d4f8d58e5a9c29a3e
peaceguard <marek.stoppel@gmail.com>
2021-03-09 19:58:41 +0100

    New task: Proste Dodawanie - preliminary version

---
+++ spoj1_proste_dod.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+# Spoj task #1 - EASY ADDITION: https://pl.spoj.com/problems/RNO_DOD/
+
+print("Please provide the number of tests")
+t = int(input())
+table = []
+total = 0
+
+for num in range (0, t*2):
+    table += list(str.split(input()))
+print(table)
+
+for elem in table:
+    total += int(elem)
+print(total)
