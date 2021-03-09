4a224ece32c3bf86bea8abd513f70b5d4e3b59d2
peaceguard <marek.stoppel@gmail.com>
2021-03-09 19:45:05 +0100

    New Task: SPOJ Połowa

---
+++ spoj2_polowa.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+# Spoj task #2 - POŁOWA: https://pl.spoj.com/problems/POL/
+
+print("Please provide the number of tests")
+t = int(input())
+table = []
+
+for num in range (0, t):
+    table += list(str.split(input()))
+print(table)
+
+half = ""
+x = 0
+y = ""
+
+for elem in table:
+    half = int(len(elem)/2)
+    for x in range(0,half):
+        y += elem[x]
+    print(y)
+    y = ""
