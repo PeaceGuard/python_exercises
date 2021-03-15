027936f86039d46c46ca324ac1474a66a4a0775a
peaceguard <marek.stoppel@gmail.com>
2021-03-15 23:27:46 +0100

    Added calculation using for loop, corrected for calculation to iterate upwards

--- 1.sum_100_nat.py
+++ 1.sum_100_nat.py
@@ -1,23 +1,37 @@
 #! /usr/bin/env python3
 
-# Sum of first 100 natural numbers
+# Sum of first 100 natural numbers, using 'while'
 
-number = 100
-sum = 0
+number = 1
+total = 0
 
-# use while loop to iterate downwards from 'number' until zero
+while number <= 100:
+    total += number
+    number += 1
+print("While: The sum of first 100 natural numbers is " + str(total) + ".")
 
-while (number > 0):
-    sum += number
-    number -= 1
-print("The sum of first 100 natural numbers is",sum,".")
-print("The sum of first 100 natural numbers is " + str(sum) + ".")
-#sum == 5050
-print("The sum of first 100 natural numbers is " + str(5050) + ".")
-print("The sum of first 100 natural numbers is 5050.")
-print("The sum of first 100 natural numbers is {} -- {} xxx {} *** {}.".format(sum, -sum, 222, "kotek"))
-print("The sum of first 100 natural {yyy} numbers is {xxx}.".format(xxx=sum, yyy="kotek"))
-print(f"The sum of first 100 natural numbers is {sum}.")
-#print(f"The sum of first 100 natural numbers is {sum}.",end="")
+# Sum of first 100 natural numbers, using 'for'
+
+tot = 0
+
+for num in range(1, 101):
+    tot += num
+print("For: The sum of first 100 natural numbers is " + str(tot) + ".")
 
 
+
+#   Different ways to use print function:
+# print("The sum of first 100 natural numbers is " + str(total) + ".")
+# print(f"The sum of first 100 natural numbers is {total}.")
+# print("The sum of first 100 natural numbers is ", total , ".", sep="")
+# print("The sum of first 100 natural numbers is {} {}.".format(-total, "kotek"))
+# print("The sum of first 100 natural {yy} numbers is {xx}.".format(xx = total, yy = "kotek"))
+
+#   Different ways to print separate list elements
+# sequence = [1, 2, 3, 4, 5, 6]
+# print(f"""The sum of first 100 natural numbers is {total} >>> {" ".join(map(str, sequence))}.""")
+# print(f'{" ".join(map(str, sequence))}')
+# sequence_elements = " ".join(map(str, sequence))
+# print(sequence_elements)
+
+#   \ sign deactivates the next sign within the code
