f248c063f9265e8008bd8f79e4570797dd590a4d
peaceguard <marek.stoppel@gmail.com>
2021-03-15 23:56:25 +0100

    Added/changed file comments

--- spoj6_ktora_godz.py
+++ spoj6_ktora_godz.py
@@ -7,18 +7,28 @@ while True:
     binary_sequence = binary_numbers.replace(" ", "")
     lst = str.split(binary_numbers)
     if str.isdigit(binary_sequence) == False:
-        print("Please provide only digits divided by spaces")
+        print("Please provide only digits")
         continue
     try:
-        if int(lst[0], 2) not in range(0, 3) or int(lst[1], 2) not in range(0, 10) or int(lst[2], 2) not in range(0, 6) or int(lst[3], 2) not in range(0, 10):
-            print("Please provide binary digits correct for hours and minutes")
+        #
+        # if expr1 not in range(z, p) or expr2 not in range(s, t):
+        # expr1, expr2 ~~~ (dowolne wyrażeie w tym zmienna) 
+        #
+        if int(lst[0], 2) not in range(0, 3) or int(lst[1], 2) not in range(0, 10) or int(lst[2], 2) not in range(0, 6) or int(lst[3], 2) not in range(0, 10) or (int(lst[0], 2) == 2 and int(lst[1], 2) not in range(0, 4)):
+            print("Please provide binary digits convertable to correct hour & minute values")
             continue
     except ValueError:
         print("Please provide only binary digits")
         continue
+    except IndexError:
+        print("Please provide four sequences of binary digits separated by spaces")
+        continue
     else:
+        # maksymalnie 25 znaków w linijce powyżej
+        # 0 0 0 0 -> 00:00 a nie 0:0
+        print(f"{h}:{m}")
         print(str(int(lst[0], 2)) + str(int(lst[1], 2)) + ":" + str(int(lst[2], 2)) + str(int(lst[3], 2)))
         break
 
-# Missing proofing for hours > 23 & minutes > 59
+# Additional exception for -numbers?
 # QUESTION: Why fail when continuing script after meeting if condition or after quit()?
