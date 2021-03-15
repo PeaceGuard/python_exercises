b8ebbf1a87d5bd917151aa9ecf372672998b9ba5
peaceguard <marek.stoppel@gmail.com>
2021-03-15 23:36:22 +0100

    Added calculation using for loop, corrected to iterate upwards and simplified the iteration

--- 2.sum_100_nat_div7.py
+++ 2.sum_100_nat_div7.py
@@ -1,17 +1,46 @@
 #! /usr/bin/env python3
 
-# Sum of first 100 natural numbers divisible by 7
+# Sum of first 100 natural numbers divisible by 7, using 'while'
 
-number = 700
-sum = 0
+total = 0
+number = 7
+i = 1
 
-# use while loop to iterate downwards from 'number' until zero & if to sum only numbers divisible by 7
+while i <= 100:
+    total += number
+    number += 7
+    i += 1
 
-while(number > 0):
-    if number % 7 == 0:
-        sum += number
-        number -= 1
-    else:
-        sum = sum
-        number -= 1
-print("The sum of first 100 natural numbers divisible by 7 is",sum,".")
+#while (number <= 700):
+#    if number % 7 == 0:
+#        total += number
+#        number += 1
+#    else:
+#        number += 1
+
+print("While: The sum of first 100 natural numbers divisible by 7 is " + str(total) + ".")
+
+# Sum of first 100 natural numbers divisible by 7, using 'for' - v1
+
+tot = 0
+num = 7
+
+for i in range(0, 100):
+    tot += num
+    num += 7
+
+# for num in range(1, 701):
+#    if num % 7 == 0:
+#        tot += num
+
+print("For#1: The sum of first 100 natural numbers divisible by 7 is " + str(tot) + ".")
+
+# Sum of first 100 natural numbers divisible by 7, using 'for' - v2
+
+tot2 = 0
+
+for num2 in range(7, 701, 7):
+    tot2 += num2
+print("For#2: The sum of first 100 natural numbers divisible by 7 is " + str(tot2) + ".")
+
+# Warunek pętli ma być zdefiniowany przez sumę 100 liczb + jeszcze coś do usunięcia (else & continue w for-if?)
